427a7f0dd33c45a601c3a0e93b226dfad8042840
mvis <mining.visualizer@gmail.com>
2017-08-17 09:49:19 -0700

    separate build scripts

---
+++ Build/build-linux.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cd ..
+
+# lazbuild --build-all --build-mode=Release MiningVisualizer.lpi
+
+# if [ $? -ge 1 ] ; then
+#    echo "."
+#    echo "."
+#    echo "."
+#    echo "compilation error"
+#    echo "."
+#    echo "."
+#    echo "."
+#    exit 1
+# fi
+
+rm -r Release/MiningVisualizer/*
+
+cp MiningVisualizer Release/MiningVisualizer
+res=$?
+
+cp -R WebApp Release/MiningVisualizer
+res=$((res + $?))
+
+cp -R SSL-Certs Release/MiningVisualizer
+res=$((res + $?))
+
+
+# tar -czf Release/MiningVisualizer-vxxx-linux.tar.gz  --directory=Release MiningVisualizer
+# res=$((res + $?))
+
+# if [ "$res" -ge 1 ] ; then
+#    echo "."
+#    echo "."
+#    echo "."
+#    echo "ERRORS WERE ENCOUNTERED!!!"
+#    echo "."
+#    echo "."
+#    echo "."
+#    exit 1
+# fi
+
+# echo "."
+# echo "."
+# echo "."
+# echo "script completed successfully"
+# echo "."
+# echo "."
+# echo "."
